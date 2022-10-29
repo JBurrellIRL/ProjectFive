17c7800f3dd41a00d34f5946f7ae38b58945733c
JBurrellIRL <jburrellirl@gmail.com>
2022-10-29 16:45:49 +0000

    CSS adjustments to products.html

--- products/models.py
+++ products/models.py
@@ -29,8 +29,8 @@ class Product(models.Model):
     condition = models.TextField()
     excerpt = models.TextField(blank=True)
     price = models.DecimalField(max_digits=6, decimal_places=2)
-    image = models.ImageField(null=True, blank=True)
     image_url = models.URLField(max_length=1024, null=True, blank=True)
+    image = models.ImageField(null=True, blank=True)
 
     def __str__(self):
         return self.name
