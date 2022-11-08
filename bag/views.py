009f6ff2d73f2ca10e432d956d6c8c8b2e87489b
JBurrellIRL <jburrellirl@gmail.com>
2022-11-08 12:57:37 +0000

    Create view to add items to the shopping bag

--- bag/views.py
+++ bag/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, reverse
 
 # Create your views here.
 
@@ -7,3 +7,17 @@ def view_bag(request):
     """ A view to return the shopping bag page """
 
     return render(request, 'bag/bag.html')
+
+
+def add_to_bag(request, item_id):
+    """
+    View to add items to the shopping bag
+    """
+    quantity = 1
+
+    bag = request.session.get('bag', {})
+    bag[item_id] = bag.get(item_id, quantity)
+
+    request.session['bag'] = bag
+    print(request.session['bag'])
+    return redirect(reverse('products'))
