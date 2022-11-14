438988d1f778719fa101f54ca1f2193104a5610c
JBurrellIRL <jburrellirl@gmail.com>
2022-11-14 17:34:33 +0000

    Install Django-countries for countryfield dropdown, adjustment to checkout layout

--- manage.py
+++ manage.py
@@ -6,7 +6,8 @@ import sys
 
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'jbs_record_store.settings')
+    os.environ.setdefault(
+        'DJANGO_SETTINGS_MODULE', 'jbs_record_store.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
