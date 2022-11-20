92e3f20e2d04a10762cc518b467dd6ffb1682a9d
JBurrellIRL <jburrellirl@gmail.com>
2022-11-20 17:25:46 +0000

    Add custom widget template for form image field

--- reviews/views.py
+++ reviews/views.py
@@ -8,7 +8,7 @@ from django.views.generic import CreateView, UpdateView, DeleteView
 from django.contrib import messages
 from django.contrib.messages.views import SuccessMessageMixin
 from django.contrib.auth.decorators import login_required
-from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
+from django.contrib.auth.mixins import LoginRequiredMixin
 
 
 def all_reviews(request):
