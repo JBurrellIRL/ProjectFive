6eb5497737c1039662ecd4c5301ae2bfb72b0499
JBurrellIRL <jburrellirl@gmail.com>
2022-11-20 22:21:54 +0000

    Add logic for display of products

--- products/views.py
+++ products/views.py
@@ -50,9 +50,11 @@ def product_detail(request, product_id):
     """ A view to show individual product details """
 
     product = get_object_or_404(Product, pk=product_id)
+    category = Category.objects.all()
 
     context = {
         'product': product,
+        'category': category,
     }
 
     return render(request, 'products/product_detail.html', context)
