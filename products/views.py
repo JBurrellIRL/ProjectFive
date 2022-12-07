314cf4a8dded27d0f7865e299512474c6ecafdd1
JBurrellIRL <jburrellirl@gmail.com>
2022-12-07 15:52:50 +0000

    Update product add/edit views error messages

--- products/views.py
+++ products/views.py
@@ -67,7 +67,7 @@ def add_product(request):
     """ View to add a product to the store """
     if not request.user.is_superuser:
         messages.error(
-            request, 'Sorry, only store administrators can do that.')
+            request, 'Sorry, only store administrators can add products.')
         return redirect(reverse('home'))
 
     if request.method == 'POST':
@@ -96,7 +96,7 @@ def edit_product(request, product_id):
     """ View to edit a product in the store """
     if not request.user.is_superuser:
         messages.error(
-            request, 'Sorry, only store administrators can do that.')
+            request, 'Sorry, only store administrators can modify products.')
         return redirect(reverse('home'))
 
     product = get_object_or_404(Product, pk=product_id)
@@ -128,7 +128,7 @@ def edit_product(request, product_id):
 def delete_product(request, product_id):
     """Delete a product from the store"""
     if not request.user.is_superuser:
-        messages.error(request, "Sorry, only store owners can do that.")
+        messages.error(request, "Sorry, only store owners can delete products")
         return redirect(reverse("home"))
 
     product = get_object_or_404(Product, pk=product_id)
